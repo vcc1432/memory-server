5dbb6e7f932085b240feea7f7964fbe0b663f2ee
vcc1432 <valerie.gersen@gmail.com>
2018-09-21 14:15:18 +0200

    temp disable column

--- src/games/entities.ts
+++ src/games/entities.ts
@@ -70,8 +70,8 @@ export class Player extends BaseEntity {
   @ManyToOne(_ => Game, game => game.players)
   game: Game
 
-  @Column()
-  userId: number
+  // @Column()
+  // userId: number
 
   @Column('char', {length: 1})
   symbol: Symbol
